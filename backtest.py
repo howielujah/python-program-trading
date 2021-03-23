eafccc22370c6055da418d3e14c2b0f2e01fdb5b
洪英皓 <hauhauster@gmail.com>
2021-03-23 15:05:12 +0800

    新增機器人, cci 搭配布林策略

---
+++ backtest.py
@@ -0,0 +1,200 @@
+from database import *
+import talib
+
+def ticks_to_kbars(ticks, interval='1Min'):
+
+    kbars = pd.DataFrame()
+    
+    kbars['open'] = ticks['close'].resample(interval).first()
+    kbars['close'] = ticks['close'].resample(interval).last()
+    kbars['high'] = ticks['close'].resample(interval).max()
+    kbars['low'] = ticks['close'].resample(interval).min()
+    kbars['volume'] = ticks['volume'].resample(interval).sum()
+    
+    kbars.dropna(inplace=True)
+    
+    return kbars
+
+
+def get_technical_indicator(kbars):
+    
+#     kbars['rsi'] = talib.RSI(kbars.close, timeperiod=14)
+    
+#     macd, macdsignal, macdhist = talib.MACD(kbars.close, fastperiod=12, slowperiod=26, signalperiod=9)
+#     kbars['macd'] = macd
+#     kbars['macdsignal'] = macdsignal
+#     kbars['macdhist'] = macdhist
+    
+    kbars['slowk'], kbars['slowd'] = talib.STOCH(kbars.high,
+                        kbars.low,
+                        kbars.close,
+                        fastk_period=9,
+                        slowk_period=3,
+                        slowk_matype=0,
+                        slowd_period=3,
+                        slowd_matype=0)
+    kbars['cci'] = talib.CCI(kbars.high, kbars.low, kbars.close, timeperiod=14)
+    
+    upper, middle, lower = talib.BBANDS(kbars.close, 
+                                        timeperiod=20, 
+                                        nbdevup=2.1, 
+                                        nbdevdn=2.1, 
+                                        # Moving average type: simple moving average here
+                                        matype=0)
+    kbars['upper'] = upper
+    kbars['middle'] = middle
+    kbars['lower'] = lower
+
+    return kbars
+
+
+def day_trading_backtest(code, date, connection, api):
+    
+    tw_calendar = get_calendar('XTAI')
+    prev_trading_date = tw_calendar.previous_close(date).date()
+    
+    ticks = get_ticks(code, prev_trading_date, connection, api)[0].append(get_ticks(code, date, connection, api)[0])
+    kbars = ticks_to_kbars(ticks, '5Min')
+    if kbars.empty:
+        return pd.DataFrame()
+    kbars = get_technical_indicator(kbars)
+    prev_close_price = kbars['close'].get(pd.to_datetime(prev_trading_date).replace(hour=13, minute=30))
+    kbars = kbars[date:]
+    
+    entry_price = 0
+    entry_time = None
+    
+    exit_price = 0
+    exit_time = None
+    
+    position = 0
+    open_price = 0
+            
+    for ts in range(len(kbars)):
+#         current_time = kbars.iloc[ts].name + pd.Timedelta(minutes=5)
+        current_time = kbars.iloc[ts].name
+        current_price = kbars['open'][ts]
+        if ts == 0: 
+            open_price = kbars['open'][ts]
+            continue
+        if ts ==1:
+            if(kbars['close'][0] >= open_price * 1.04):
+                break
+            continue
+        
+        if (
+            current_time <= date.replace(hour=10, minute=30, second=0) and
+            kbars.iloc[ts-2]['cci'] < -350 and
+            kbars.iloc[ts-2]['slowk'] < kbars.iloc[ts-2]['slowd'] and
+            kbars.iloc[ts-1]['slowk'] > kbars.iloc[ts-1]['slowd'] and  
+            position == 0
+        ):
+            if current_price < 20:
+                position = 20
+            else:
+                position = 2          
+            entry_price = current_price
+            entry_time = current_time.time()
+
+            print('[{}] buy {} at {}'.format(current_time, code, current_price))
+        
+        elif (
+            (current_price > open_price * 1.095 and position != 0) or
+            (current_price >= entry_price * 1.02 and kbars.iloc[ts-1]['cci'] <= 130 and position != 0) or
+#             (current_price <= entry_price * 0.98 and position != 0) or
+            (
+            kbars.iloc[ts-1]['cci'] > 130 and
+            position != 0)
+        ):
+            exit_price = current_price
+            exit_time = current_time.time()
+            print('[{}] sell {} at {}'.format(current_time, code, current_price))
+            
+            break
+            
+        elif (
+            current_time >= date.replace(hour=13, minute=20, second=0) and
+            position != 0
+        ):
+            
+            exit_price = current_price
+            exit_time = current_time.time()
+            
+            print('[{}] sell {} at {}'.format(current_time, code, current_price))
+            
+            break
+            
+    if entry_time and exit_time:
+        transaction = pd.DataFrame([[date, 
+                                     code, 
+                                     entry_time,
+                                     entry_price, 
+                                     position * 1000,
+                                     entry_price * position * 1000,
+                                     exit_time,
+                                     exit_price,
+                                     position * 1000,
+                                     exit_price * position * 1000]
+                                   ],
+                                    columns=[
+                                        '成交日期', 
+                                        '股票代號',
+                                        '買進時間',
+                                        '買進價格',
+                                        '買進股數',
+                                        '買進金額',
+                                        '賣出時間',
+                                        '賣出價格',
+                                        '賣出股數',
+                                        '賣出金額'])
+        return transaction
+    else:
+        return pd.DataFrame()
+    
+def get_trading_codes(codes, connection, api):
+    trading_codes = []
+    for code in codes:
+        stock = get_stock(code, connection, api)
+        if not stock[0].empty and stock[0].iloc[0]['day_trade'] == 'Yes':
+            trading_codes.append(code)
+    return trading_codes
+
+def backtest(start_date, end_date, connection, api, discount=0.38):
+    
+    tw_calendar = get_calendar('XTAI')
+    
+    transactions = pd.DataFrame()
+    
+    for date in pd.date_range(start_date, end_date):
+
+        if date not in tw_calendar.opens:
+            continue
+        
+        codes = get_stocks(date, connection)
+        day_trading_codes = get_trading_codes(codes, connection, api)
+        
+        for code in day_trading_codes:
+            
+            transaction = day_trading_backtest(code, pd.to_datetime(date), connection, api)
+            
+            if not transaction.empty:
+                transactions = transactions.append(transaction)
+                
+    if not transactions.empty:
+
+        transactions['買進手續費'] = transactions['買進金額'] * 0.001425 * discount
+        transactions['買進手續費'] = transactions['買進手續費'].apply(lambda fee : fee if fee > 20 else 20)
+        transactions['買進手續費'] = transactions['買進手續費'].astype(int)
+
+        transactions['賣出手續費'] = transactions['賣出金額'] * 0.001425 * discount
+        transactions['賣出手續費'] = transactions['賣出手續費'].apply(lambda fee : fee if fee > 20 else 20)
+        transactions['賣出手續費'] = transactions['賣出手續費'].astype(int)
+
+        transactions['交易稅'] = transactions['賣出金額'] * 0.0015
+        transactions['交易稅'] = transactions['交易稅'].astype(int)
+
+        transactions['損益'] = (transactions['賣出金額'] - transactions['買進金額']) - (transactions['買進手續費'] + transactions['賣出手續費'] + transactions['交易稅'])
+
+        transactions = transactions.set_index(['成交日期'])
+        
+    return transactions
